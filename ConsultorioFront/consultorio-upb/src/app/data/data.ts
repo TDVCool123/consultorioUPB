72be05a1d0cdf359d406c9a322e99d54b173ca62
TDVCool123 <luisandypp@gmail.com>
2024-01-12 18:40:04 -0400

    Commit 4

--- ConsultorioFront/consultorio-upb/src/app/data/data.ts
+++ ConsultorioFront/consultorio-upb/src/app/data/data.ts
@@ -1,15 +1,315 @@
 import fb from "../images/facebookIcon.png"
 import ig from "../images/InstagramIcon.png"
-
 export const socialLinks = [
     {
       id: 1,
-      link: "facebook",
+      link: "https://www.facebook.com/StudentLifeUPBLP",
       icon: fb
     },
     {
       id: 2,
-      link: "instagram",
+      link: "https://www.facebook.com/StudentLifeUPBLP",
       icon: ig
     }
   ];
+
+export const VIDEOS = [
+  {
+    id: 1,
+    tittle:"TÉCNICAS DE ESTUDIO",
+    videoSrc:"TÉCNICAS DE ESTUDIO.mp4",
+    description:"Aprende de los estudiantes de la UPB cuales son los metodos que ellos usan para estudiar y sacar buenas notas "
+  }
+]
+
+
+export const QUESTIONS = [
+  {
+    "id":1,
+    "statement": "¿Tieness claras las razones por las que estudias?",
+    "group": "A"
+  },
+  {
+    "id":2,
+    "statement": "Sueles cambiar con cierta frecuencia el lugar donde estudias en tu casa?",
+    "group": "B"
+  },
+  {
+    "id":3,
+    "statement": "Procuras estudiar en aquellas horas en que te encuentras en mejores condiciones para aprender?",
+    "group": "C"
+  },
+  {
+    "id":4,
+    "statement": "¿Te has parado a pensar sobre el numero de actividades que realizas cada dia y el tiempo que le dedicas a cada una de ellas?",
+    "group": "D"
+  },
+  {
+    "id":5,
+    "statement": "¿Acostumbras a mirar el indice y los apartados mas importantes de un tema antes de coomenzar a estudiar?",
+    "group": "E"
+  },
+  {
+    "id":6,
+    "statement": "¿Tomas nota de las ecplicaciones de los profesores?",
+    "group": "A"
+  },
+  {
+    "id":7,
+    "statement": "¿Lees con detenimiento los enunciados de las preguntas?",
+    "group": "F"
+  },
+  {
+    "id":8,
+    "statement": "¿Consideras el estudio una ocacion para aprender?",
+    "group": "A"
+  },
+  {
+    "id":9,
+    "statement": "¿Tu lugar de estudio esta alejado de ruidos y otras cosa que impidan concentrarte?",
+    "group": "B"
+  },
+  {
+    "id":10,
+    "statement": "Antes de empezar tu trabajo,¿Haces un esquema de los aspectos mas importantes que vas a desarrollar?",
+    "group": "G"
+  },
+  {
+    "id":11,
+    "statement": "¿Sueles dormir, por lo menos, 8 horas cada día?",
+    "group": "C"
+  },
+  {
+    "id":12,
+    "statement": "¿Tienes una idea general de lo que vas a estudiar a lo largo del curso en cada materia o asignatura?",
+    "group": "D"
+  },
+  {
+    "id":13,
+    "statement": "Antes de estudiar el tema con profundidad, ¿realizas una lectura rápida del mismo para hacerte una idea general?",
+    "group": "E"
+  },
+  {
+    "id":14,
+    "statement": "Antes de escribir la respuesta, ¿piensas detenidamente lo que vas a contestar y cómo lo vas a hacer?",
+    "group": "F"
+  },
+  {
+    "id":15,
+    "statement": "Cuando comienzas a estudiar, ¿tardas bastante tiempo en concentrarte?",
+    "group": "A"
+  },
+  {
+    "id":16,
+    "statement": "En el lugar donde estudias habitualmente, ¿hay personas o cosas que distraen tu atención?",
+    "group": "B"
+  },
+  {
+    "id":17,
+    "statement": "Cuando tomas notas, ¿sueles copiar al pie de la letra lo que dice el profesor?",
+    "group": "E"
+  },
+  {
+    "id":18,
+    "statement": "¿Sueles dormir mal y por la mañana te sientes cansado y poco repuesto?",
+    "group": "C"
+  },
+  {
+    "id":19,
+    "statement": "¿Has elaborado un plan de trabajo en función del tiempo de que dispones y de las asignaturas que tienes?",
+    "group": "D"
+  },
+  {
+    "id":20,
+    "statement": "Cuando has de hacer un trabajo, ¿sueles comentar con tu profesor el esquema y desarrollo del mismo?",
+    "group": "G"
+  },
+  {
+    "id":21,
+    "statement": "Después de una primera lectura del tema, ¿haces una lectura lenta y reposada para buscar las ideas más importantes? ",
+    "group": "E"
+  },
+  {
+    "id":22,
+    "statement": "Cuando faltas a clase, ¿sueles informarte a través de un compañero o del profesor de lo que se ha realizado y se ha de realizar?",
+    "group": "A"
+  },
+  {
+    "id":23,
+    "statement": "En un examen o ejercicio, ¿repartes el tiempo para cada pregunta?",
+    "group": "F"
+  },
+  {
+    "id":24,
+    "statement": "Cuando no comprendes algo, ¿lo anotas para luego consultarlo?",
+    "group": "A"
+  },
+  {
+    "id":25,
+    "statement": "¿Tienes luz suficiente (luz natural o lámpara para estudiar sin forzar la vista?",
+    "group": "B"
+  },
+  {
+    "id":26,
+    "statement": "¿Combinas el tiempo que dedicas al estudio con el tiempo de descanso? ",
+    "group": "C"
+  },
+  {
+    "id":27,
+    "statement": "¿Dedicas a cada asignatura el tiempo necesario que pueda asegurarte un buen resultado?",
+    "group": "D"
+  
+  },
+  {
+    "id":28,
+    "statement": "¿Subrayas las ideas más importantes a medida que vas estudiando un tema?",
+    "group": "E"
+  
+  },
+  {
+    "id": 29,
+    "statement": "¿Sueles abrir un poco la puerta/ventana de tu habitación de estudio para que se ventile?",
+    "group": "B"
+    },
+    {
+    "id": 30,
+    "statement": "En el caso que necesites información para hacer un trabajo, ¿sabes cómo encontrarla?",
+    "group": "G"
+    },
+    {
+    "id": 31,
+    "statement": "¿Cuidas de que tu expresión escrita sea clara, ordenada y comprensiva?",
+    "group": "F"
+    },
+    {
+    "id": 32,
+    "statement": "¿Tratas de estudiar sólo lo justo para una prueba o control?",
+    "group": "A"
+    },
+    {
+    "id": 33,
+    "statement": "¿Has notado que los resultados en tus estudios son bajos cuando tienes el tiempo demasiado ocupado en otras cosas?",
+    "group": "C"
+    },
+    {
+    "id": 34,
+    "statement": "¿Sigues el plan de trabajo que te has propuesto desde el principio del curso?",
+    "group": "D"
+    },
+    {
+    "id": 35,
+    "statement": "En tu lugar de estudio, ¿dispones de suficiente espacio para tener organizado y a mano todo el material que necesitas?",
+    "group": "B"
+    },
+    {
+    "id": 36,
+    "statement": "Antes de empezar a estudiar, ¿piensas lo que vas a hacer y cómo vas a distribuir el tiempo?",
+    "group": "D"
+    },
+    {
+    "id": 37,
+    "statement": "Resumes lo más importante de cada uno de los apartados del tema, para elaborar después una síntesis general?",
+    "group": "E"
+    },
+    {
+    "id": 38,
+    "statement": "¿Cabe en tu mesa todo lo que necesitas para el estudio?",
+    "group": "B"
+    },
+    {
+    "id": 39,
+    "statement": "Antes de empezar tu trabajo, ¿Haces un esquema de los aspectos más importantes que vas a desarrollar? Cuando buscas información en un libro, enciclopedia, etc., para realizar un trabajo, ¿te limitas a copiar al pie de la letra lo que lees?",
+    "group": "G"
+    },
+    {
+    "id": 40,
+    "statement": "¿Sueles interrumpir tus sesiones de estudio en casa?",
+    "group": "D"
+    },
+    {
+    "id": 41,
+    "statement": "¿Te has acostumbrado a hacer esquemas, croquis, cuadros, gráficos, etc., cuando estudias un tema?",
+    "group": "E"
+    },
+    {
+    "id": 42,
+    "statement": "¿Intentas sobreponerte con interés, con ánimo, ante un bajón en las notas?",
+    "group": "A"
+    },
+    {
+    "id": 43,
+    "statement": "¿A tu silla de estudio le falta respaldo?",
+    "group": "B"
+    },
+    {
+    "id": 44,
+    "statement": "¿Tienes organizado todo el material que se ha trabajado en cada materia?",
+    "group": "D"
+    },
+    {
+    "id": 45,
+    "statement": "¿La altura de tu silla de estudio te permite apoyar bien los pies en el suelo?",
+    "group": "B"
+    },
+    {
+    "id": 46,
+    "statement": "Cuando terminas tu sesión de estudio personal, ¿sueles acabar las tareas que te habías propuesto?",
+    "group": "A"
+    },
+    {
+    "id": 47,
+    "statement": "¿La altura de la mesa está proporcionada a la silla?",
+    "group": "B"
+    },
+    {
+    "id": 48,
+    "statement": "¿Tienes la costumbre de preparar los exámenes con poco tiempo de antelación?",
+    "group": "D"
+    },
+    {
+    "id": 49,
+    "statement": "¿Relacionas el tema estudiado con lo aprendido anteriormente?",
+    "group": "E"
+    },
+    {
+    "id": 50,
+    "statement": "¿Descuidas la redacción y presentación del trabajo?",
+    "group": "G"
+    },
+    {
+    "id": 51,
+    "statement": "¿Acostumbras a memorizar las ideas más importantes que has resumido en un tema o lección?",
+    "group": "E"
+    },
+    {
+    "id": 52,
+    "statement": "¿Pones de tu parte todo lo que puedes para asegurarte unos buenos resultados en tu tarea escolar?",
+    "group": "A"
+    },
+    {
+    "id": 53,
+    "statement": "¿Te acercas excesivamente sobre el libro cuando estudias?",
+    "group": "C"
+    },
+    {
+    "id": 54,
+    "statement": "¿Aprovechas algún momento del fin de semana para repasar aquellos temas que te han quedado más flojos?",
+    "group": "D"
+    },
+    {
+    "id": 55,
+    "statement": "Si te sobra tiempo, ¿entregas el examen inmediatamente sin repasar de nuevo las respuestas?",
+    "group": "F"
+    },
+    {
+    "id": 56,
+    "statement": "¿Sueles indicar el nombre de todos aquellos materiales (libros, enciclopedias, revistas, etc.) que has utilizado en el trabajo?",
+    "group": "G"
+    }
+  
+
+
+
+]
+
+
