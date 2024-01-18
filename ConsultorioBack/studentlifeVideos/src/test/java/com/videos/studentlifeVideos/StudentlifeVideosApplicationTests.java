b07a04a96b50ea38f931ef6fe3b6e3eeaba365f4
TDVCool123 <luisandypp@gmail.com>
2024-01-17 23:14:19 -0400

    Iniacilazando el back de los videos

---
+++ ConsultorioBack/studentlifeVideos/src/test/java/com/videos/studentlifeVideos/StudentlifeVideosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.videos.studentlifeVideos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentlifeVideosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
